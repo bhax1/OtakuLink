6a942413a09da716dfacecc1ba1b9518f234cc6e
bhax1 <b.ongoy.534716@umindanao.edu.ph>
2024-11-17 20:27:10 +0800

    Add logo to the app
    Added the logo image to the assets and updated the app's UI to display it.

--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'OtakuLink',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 255, 255, 255)),
         useMaterial3: true,
       ),
       home: Authenticator(),
